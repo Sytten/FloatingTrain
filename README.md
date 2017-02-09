7eb20101b34b61ea636719ea5c3bbcfe0a5d73c6
Unknown <jean.pascal.mcgee@gmail.com>
2017-02-09 13:29:02 -0500

    Added documentation
    Added naming conventions as established in client specifications to the ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # FloatingTrain
+
+Voici la nomenclature à utiliser dans le code MATLAB/Simulink:
+
+masseS : masse de la sphère
+masseP : masse de la plaque
+inertiePx : inertie de la plaque autour de l’axe x
+inertiePy : inertie de la plaque autour de l’axe y
+XK, YK, ZK : position inertielle des éléments fixes K (K = A, B, C, D, E, F)
+Px, Py, Pz : position de la sphère dans le repère inertiel
+Vx, Vy, Vz : vitesse de la sphère dans le repère inertiel
+Ax, Ay : angle de rotation de la plaque ϕ autour de l’axe ܫԦ
+௫ et θ autour de l’axe ܫԦ
+௬
+Wx, Wy : vitesse angulaire de la plaque Wx autour de l’axe ܫԦ
+௫ et Wy autour de l’axe ܫԦ
+௬
+FA, FB, FC : forces appliquée par les actionneurs sur la plaque (positif vers le bas)
+MA, MB, MC : couple appliquée par les actionneurs sur la plaque
+VA, VB, VC : tension électrique appliquée aux actionneurs
+IA, IB, IC : courant électrique dans les actionneurs
+∗ _݉݁ݏ : réfère à des variables mesurées
+∗ _݀݁ݏ : réfère aux variables désirées ou commandées
+∗ _݅݊݅ : réfère aux variables initiales
+∗ _݂݅݊ : réfère aux variables finales
+∗ _݁ݍ : réfère aux variables à l’équilibre
--- matlab/sm_7.m
+++ matlab/sm_7.m
@@ -1,28 +1,30 @@
 % Auteur : Jordan Careau-Beaulieu, Jean-Pascal McGee
-% Date de création : 09 février 2017
-% Date d'édition : 09 février 2017
-% Description du programme : Calcul de l'accélération de la bille en
+% Date de crï¿½ation : 09 fï¿½vrier 2017
+% Date d'ï¿½dition : 09 fï¿½vrier 2017
+% Description du programme : Calcul de l'accï¿½lï¿½ration de la bille en
 % fonction de l'angle de la plaque
 
 clear all
 clc
 close all
 
-
-g = 9.81;               % Accélération gravitationnelle.
-masseS = 8/1000;        % Masse de la sphère en kilogrammes.
+% DÃ©claration des constantes
+g = 9.81;               % Accï¿½lï¿½ration gravitationnelle.
+masseS = 8/1000;        % Masse de la sphï¿½re en kilogrammes.
 masseP = 425/1000;      % Masse de la plaque en kilogrammes.
-rayon_sphere = 3.9/1000;    % Rayon de la sphère en m.
+rayon_sphere = 3.9/1000;    % Rayon de la sphï¿½re en m.
 inertiePx = 1169.1/(1000^2); % Interie de la plaque en kg*m^2
 inertiePy = inertiePx;
-inertieS = (2*masseS*(rayon_sphere^2))/5; % Inertie de la sphère en kg*m^2
+inertieS = (2*masseS*(rayon_sphere^2))/5; % Inertie de la sphï¿½re en kg*m^2
 
+
+% Initialisation des variables
 delta_phi = 0.01;
 phi_min = -5.0;
 phi_max = 5.0;
 phi = [phi_min:delta_phi:phi_max]';
 
-
+% Acceleration X et Y en fonction des angles Phi et Theta
 accelerationY = -(masseS*g.*degtorad(phi))/(masseS + (inertieS/rayon_sphere^2));
 accelerationX = accelerationY;
 
@@ -30,15 +32,15 @@ figure;
 subplot(2,1,1);
 plot(phi, accelerationY);
 grid on;
-xlabel('Angle \phi (degré)');
-ylabel('Accélération m/s^2');
-title('Accéleration en Y de la bille en fonction de l''angle de la plaque.');
+xlabel('Angle \phi (degre)');
+ylabel('Acceleration m/s^2');
+title('Acceleration en Y de la bille en fonction de l''angle de la plaque.');
 subplot(2,1,2);
 plot(phi, accelerationX);
 grid on;
-xlabel('Angle \theta (degré)');
-ylabel('Accélération m/s^2');
-title('Accéleration en X de la bille en fonction de l''angle de la plaque.');
+xlabel('Angle \theta (degre)');
+ylabel('Acceleration m/s^2');
+title('Acceleration en X de la bille en fonction de l''angle de la plaque.');
 
 
 
