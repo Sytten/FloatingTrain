f5e53d799964ef5f32c8bb84c9770d995cd48957
Julien Larochelle <julien.larochelle@usherbrooke.ca>
2017-02-27 13:56:47 -0500

    Cleared the code, added axes titles

--- matlab/SC-1/fitting_polynomial.m
+++ matlab/SC-1/fitting_polynomial.m
@@ -1,35 +1,52 @@
+% Auteurs : Julien Larochelle, Émilie Fugulin et Philippe Girard 
+% Date de création : 20 fevrier 2017 
+% Date d'édition : 27 fevrier 2017
+% Description du programme : Approximation de données bruitées à l'aide de
+% polynome. 
+
 clear all
 close all
 clc
 
+% Chargé les données en mémoire
 load mesures_forces/Fs.mat
 load mesures_forces/Fe_attraction.mat
 
-% Fs Approximation
-N = 150;
+% Calculs pour l'approximation de Fs 
+
+N = 150; % Nombre de points utilisés 
 Fs_sub = Fs(1:N);
 z = z_pos(1:N);
 
 Fs_prime = -1 ./ Fs_sub;
 [Y, RMS, COR] = poly_approx(z, Fs_prime, 3);
 
-disp('Fs : as0   as1   as2   as3')
+disp('Coefficient pour Fs')
+disp('Fs(z) = as0 + as1*z + as2*z^2 + as3*z^3')
 disp(['Poly : ', num2str(Y')])
 disp(['RMS : ', num2str(RMS)])
 disp(['COR : ', num2str(COR)])
+disp(' ')
+disp(' ')
 
 figure
 hold on
 plot(z,Fs_sub, 'o')
 y = polyval(fliplr(Y'),z);
 plot(z, -1 ./ y)
-title('Fs')
+xlabel('Distance z (m)')
+ylabel ('Force Fs (N)')
+title('Force Fs en fonction de la distance z')
+legend('Points Fs','Droite Fs')
+legend('Location','southeast')
 hold off
 
-% Fe Approximation 1A
-N = 150;
-b = 13.029359254409743;
-i = -1;
+% Calculs pour l'approximation de Fe
+
+% Calculs pour l'approximation de Fe 1A
+N = 150; % Nombre de points utilisés 
+b = 13.029359254409743; % Constante
+i = -1; % Courant
 C1 = sign(i)*(i^2 + b*abs(i));
 Fe1_sub = Fe_m1A(1:N);
 z1 = z_m1A(1:N);
@@ -37,23 +54,11 @@ z1 = z_m1A(1:N);
 Fe1_prime = C1 ./ Fe1_sub;
 [Y1, RMS, COR] = poly_approx(z1, Fe1_prime, 3);
 
-disp('Fe_m1A : ae0   ae1   ae2   ae3')
-disp(['Poly : ', num2str(Y1')])
-disp(['RMS : ', num2str(RMS)])
-disp(['COR : ', num2str(COR)])
 
-figure
-hold on
-plot(z1,Fe1_sub, 'o')
-y = polyval(fliplr(Y1'),z1);
-plot(z1, C1 ./ y)
-title('Fe m1A')
-hold off
-
-% Fe Approximation 2A
-N = 150;
-b = 13.029359254409743;
-i = -2;
+% Calculs pour l'approximation de Fe 1A
+N = 150; % Nombre de points utilisés 
+b = 13.029359254409743; % Constante
+i = -2; % Courant
 C2 = sign(i)*(i^2 + b*abs(i));
 Fe2_sub = Fe_m2A(1:N);
 z2 = z_m2A(1:N);
@@ -61,31 +66,21 @@ z2 = z_m2A(1:N);
 Fe2_prime = C2 ./ Fe2_sub;
 [Y2, RMS, COR] = poly_approx(z2, Fe2_prime, 3);
 
-disp('Fe_m2A : ae0   ae1   ae2   ae3')
-disp(['Poly : ', num2str(Y2')])
-disp(['RMS : ', num2str(RMS)])
-disp(['COR : ', num2str(COR)])
-
-figure
-hold on
-plot(z2,Fe2_sub, 'o')
-y = polyval(fliplr(Y2'),z2);
-plot(z2, C2 ./ y)
-title('Fe m2A')
-hold off
-
-% Average curves
+% Moyenne des deux courbes
 Y = (Y1 + Y2)./2;
-disp('Average Fe : ae0   ae1   ae2   ae3')
+disp('Coefficient pour Fe')
+disp('Fe(z) = ae0 + ae1*z + ae2*z^2 + ae3*z^3')
 disp(['Poly : ', num2str(Y')])
-
+disp(' ')
+disp(' ')
 [RMS, COR] = poly_rms_cor(z1, Fe1_prime, fliplr(Y'));
-disp('Fe_m1A with average curve')
+disp('Fe_m1A avec la moyenne des coefficients')
 disp(['RMS : ', num2str(RMS)])
 disp(['COR : ', num2str(COR)])
-
+disp(' ')
+disp(' ')
 [RMS, COR] = poly_rms_cor(z2, Fe2_prime, fliplr(Y'));
-disp('Fe_m2A with average curve')
+disp('Fe_m2A avec la moyenne des coefficients')
 disp(['RMS : ', num2str(RMS)])
 disp(['COR : ', num2str(COR)])
 
@@ -94,8 +89,12 @@ hold on
 plot(z1,Fe1_sub, 'o')
 y = polyval(fliplr(Y'),z1);
 plot(z1, C1 ./ y)
-
 plot(z2,Fe2_sub, 'o')
 y = polyval(fliplr(Y'),z2);
 plot(z2, C2 ./ y)
+xlabel('Distance z (m)')
+ylabel ('Force Fe (N)')
+title('Force Fe en fonction de la distance z')
+legend('Points Fe 1A','Droite Fe 1a','Points Fe 2A','Droite Fe 2a')
+legend('Location','southeast')
 hold off
