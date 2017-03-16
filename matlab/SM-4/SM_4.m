473b33374899cd2a9bdbbdc33405ccc20afa36e1
DESKTOP-27K6JKG\Julien <julien.larochelle75@gmail.com>
2017-03-16 19:20:34 -0400

    Rename the file (it didn't work in matlab)

--- matlab/SM-4/SM_4.m
+++ matlab/SM-4/SM_4.m
@@ -21,16 +21,25 @@ ae1 = 366.4392052;
 ae2 = -30.42916186;
 ae3 = 787041.4323;
 
-r_ABC = 95.2/1000; % en mm
+r_ABC = 95.2; % en mm
+r_DEF = 80; % en mm
 XA = r_ABC;
 XB = -r_ABC*sind(30);
 XC = -r_ABC*sind(30);
+XD = r_DEF*cosd(30);
+XE = -r_DEF;
+XF = r_DEF*sind(30);
 YA = 0;
 YB = r_ABC*cosd(30);
 YC = -r_ABC*cosd(30);
+YD = r_DEF*cosd(30);
+YE = 0;
+YF = -r_DEF*cosd(30);
 ZA = 0;
 ZB = 0;
-XC = 0;
+ZC = 0;
+
+
 
 dFa_dia_e = 1;
 dFb_dib_e = 1;
@@ -53,16 +62,38 @@ ra_la = Ra / La;
 rb_lb = Rb / Lb;
 rc_lc = Rc / Lc;
 
+masseS = 8/1000;        % Masse de la sphere en kilogrammes.
+masseP = 425/1000;      % Masse de la plaque
+
 Ip = 1169.1; % kg*mm^2
 
-A13_13 = [0,0,0,1,0,0,0,0,0;
+A9_9 = [0,0,0,1,0,0,0,0,0;
           0,0,0,0,1,0,0,0,0;
           0,0,0,0,0,1,0,0,0;
           (dFa_dPhi_e2*(YA^2+YB^2+YC^2))/Ip,0,0,0,0,0,dFa_dia_e/Ip,0,0;
           0,(dFa_dPhi_e2*(XA^2+XB^2+XC^2))/Ip,0,0,0,0,0,dFa_dia_e/Ip,0;
-          ]
+          0,0,(dFa_dPhi_e2*3)/(masseS+masseP),0,0,0,0,0,dFa_dia_e/(masseS+masseP);
+          0,0,0,0,0,0,-Ra/La,0,0;
+          0,0,0,0,0,0,0,-Rb/Lb,0;
+          0,0,0,0,0,0,0,0,-Rc/Lc;];
     
-C = [mTTdef m0_3x3 m0_3x4 m0_3x3;
-      m0_4x3 m0_4x3 m1_4x4 m0_4x3];
+
+B3_9 = [0,0,0;
+        0,0,0;
+        0,0,0;
+        0,0,0;
+        0,0,0;
+        0,0,0;
+        1/La,0,0;
+        0,1/Lb,0;
+        0,0,1/Lc];
+      
+C = [YD,-XD,1,0,0,0,0,0,0,0,0,0,0;
+     YE,-XE,1,0,0,0,0,0,0,0,0,0,0;
+     YF,-XF,1,0,0,0,0,0,0,0,0,0,0;
+     0,0,0,0,0,0,1,1,1,1,0,0,0;
+     0,0,0,0,0,0,1,1,1,1,0,0,0;
+     0,0,0,0,0,0,1,1,1,1,0,0,0;
+     0,0,0,0,0,0,1,1,1,1,0,0,0;];    
 
 D = zeros(7,3);
