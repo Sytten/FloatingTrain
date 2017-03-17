37eefbbf958322068ebe776ee454740cba20fd00
DESKTOP-27K6JKG\Julien <julien.larochelle75@gmail.com>
2017-03-16 21:42:38 -0400

    SM4 will be done one day

--- matlab/SM-4/SM_4.m
+++ matlab/SM-4/SM_4.m
@@ -62,23 +62,36 @@ ra_la = Ra / La;
 rb_lb = Rb / Lb;
 rc_lc = Rc / Lc;
 
+g = 9.81;               % Acceleration gravitationnelle.
 masseS = 8/1000;        % Masse de la sphere en kilogrammes.
-masseP = 425/1000;      % Masse de la plaque
+masseP = 425/1000;      % Masse de la plaque en kilogrammes.
+rayon_sphere = 3.9/1000;    % Rayon de la sphere en m.
+inertiePx = 1169.1/(1000^2); % Inertie de la plaque en kg*m^2
+inertiePy = inertiePx;
+inertieS = (2*masseS*(rayon_sphere^2))/5; % Inertie de la sphere en kg*m^2
 
 Ip = 1169.1; % kg*mm^2
-
-A9_9 = [0,0,0,1,0,0,0,0,0;
-        0,0,0,0,1,0,0,0,0;
-        0,0,0,0,0,1,0,0,0;
-        (dFa_dPhi_e2*(YA^2+YB^2+YC^2))/Ip,0,0,0,0,0,dFa_dia_e/Ip,0,0;
-        0,(dFa_dPhi_e2*(XA^2+XB^2+XC^2))/Ip,0,0,0,0,0,dFa_dia_e/Ip,0;
-        0,0,(dFa_dPhi_e2*3)/(masseS+masseP),0,0,0,0,0,dFa_dia_e/(masseS+masseP);
-        0,0,0,0,0,0,-Ra/La,0,0;
-        0,0,0,0,0,0,0,-Rb/Lb,0;
-        0,0,0,0,0,0,0,0,-Rc/Lc;];
+%% Systeme plaque
+A13_13 = [0,0,0,1,0,0,0,0,0,0,0,0,0;
+          0,0,0,0,1,0,0,0,0,0,0,0,0;
+          0,0,0,0,0,1,0,0,0,0,0,0,0;
+        (dFa_dPhi_e2*(YA^2+YB^2+YC^2))/Ip,0,0,0,0,0,0,0,0,0,dFa_dia_e/Ip,0,0;
+        0,(dFa_dPhi_e2*(XA^2+XB^2+XC^2))/Ip,0,0,0,0,0,0,0,0,0,dFa_dia_e/Ip,0;
+        0,0,(dFa_dPhi_e2*3)/(masseS+masseP),0,0,0,0,0,0,0,0,0,dFa_dia_e/(masseS+masseP);
+        0,0,0,0,0,0,0,0,1,0,0,0,0;
+        0,0,0,0,0,0,0,0,0,1,0,0,0;
+        0,-masseS*g/(masseS+inertieS/rayon_sphere^2),0,0,0,0,0,0,0,0,0,0,0;
+        masseS*g/(masseS+inertieS/rayon_sphere^2),0,0,0,0,0,0,0,0,0,0,0,0;
+        0,0,0,0,0,0,0,0,0,0,-Ra/La,0,0;
+        0,0,0,0,0,0,0,0,0,0,0,-Rb/Lb,0;
+        0,0,0,0,0,0,0,0,0,0,0,0,-Rc/Lc;];
     
 
-B3_9 = [0,0,0;
+B13_3 = [0,0,0;
+        0,0,0;
+        0,0,0;
+        0,0,0;
+        0,0,0;
         0,0,0;
         0,0,0;
         0,0,0;
@@ -91,9 +104,44 @@ B3_9 = [0,0,0;
 C = [YD,-XD,1,0,0,0,0,0,0,0,0,0,0;
      YE,-XE,1,0,0,0,0,0,0,0,0,0,0;
      YF,-XF,1,0,0,0,0,0,0,0,0,0,0;
-     0,0,0,0,0,0,1,1,1,1,0,0,0;
-     0,0,0,0,0,0,1,1,1,1,0,0,0;
-     0,0,0,0,0,0,1,1,1,1,0,0,0;
-     0,0,0,0,0,0,1,1,1,1,0,0,0;];    
+     0,0,0,0,0,0,1,0,0,0,0,0,0;
+     0,0,0,0,0,0,0,1,0,0,0,0,0;
+     0,0,0,0,0,0,0,0,1,0,0,0,0;
+     0,0,0,0,0,0,0,0,0,1,0,0,0;];    
 
 D = zeros(7,3);
+
+Aphi = [A13_13([1 4 11],1),A13_13([1 4 11],4),A13_13([1 4 11],11)];
+
+Atheta = [A13_13([2 5 12],2),A13_13([2 5 12],5),A13_13([2 5 12],12)];
+
+Az = [A13_13([3 6 13],3),A13_13([3 6 13],6),A13_13([3 6 13],13)];
+
+AxS = [A13_13([7 9],7),A13_13([7 9],9)]
+
+AyS = [A13_13([8 10],8),A13_13([8 10],10)]
+
+Bphi = [B13_3([1 4 11],1)];
+
+Btheta = [B13_3([2 5 12],2)];
+
+Bz = [B13_3([3 6 13],3)];
+
+BxS = [B13_3([7 9],1)];
+
+% to do :petites matrices c et d 
+% phi est l'entree de y_sphere
+%theta est l'entree de x_sphere
+
+
+%Cphi = [];
+
+%tf_phi =ss2tf(Aphi, Bphi)
+
+
+
+
+
+
+
+
