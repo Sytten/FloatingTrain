62eb6ef839c6337671769cd5fa0c1359d7216b16
Jean-Pascal <jean.pascal.mcgee@gmail.com>
2017-03-19 13:49:41 -0400

    Comparaison Xcorr2_Fft2

--- matlab/SI-1/Si_1.m
+++ matlab/SI-1/Si_1.m
@@ -21,8 +21,8 @@ posY = 142;
 Image = Image(posX-100:posX+100,posY-100:posX+100,1);%(Image(:,:,1)/3+Image(:,:,2)/3+Image(:,:,3)/3);
 Bille = Bille(:,:,1);%(Bille(:,:,1)/3+Bille(:,:,2)/3+Bille(:,:,3)/3);
 %Correlation 2D Normalise
-%c = normxcorr2(Bille,Image);
-c = normxcorr2_mex(double(Bille), double(Image));
+c = normxcorr2(Bille,Image);
+%c = normxcorr2_mex(double(Bille), double(Image));
 toc
 figure, surf(c), shading flat %Afficher le resultat de la correlation
 
@@ -37,6 +37,22 @@ hAx  = axes;
 imshow(Image,'Parent', hAx);
 imrect(hAx, [xoffSet+1, yoffSet+1, size(Bille,2), size(Bille,1)]);
 
+%% FFT correlation
+% Matrix dimensions
+adim = size(Image);
+bdim = size(Bille);
+% Cross-correlation dimension
+tic
+cdim = adim+bdim-1;
 
- 
+bpad = zeros(cdim);
+apad = zeros(cdim);
+
+apad(1:adim(1),1:adim(2)) = Image;
+bpad(1:bdim(1),1:bdim(2)) = Bille(end:-1:1,end:-1:1);
+ffta = fft2(apad);
+fftb = fft2(bpad);
+c = real(ifft2(ffta.*fftb));
+toc
+figure, surf(c), shading flat %Afficher le resultat de la correlation
  
