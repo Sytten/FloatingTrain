402c95405d28e4bd0d6b7885558c79aa8414f2e4
DESKTOP-27K6JKG\Julien <julien.larochelle75@gmail.com>
2017-03-19 10:44:30 -0400

    Last version

--- matlab/SM-4/SM_4.m
+++ matlab/SM-4/SM_4.m
@@ -1,6 +1,6 @@
 % Auteur : Julien Larochelle, Antoine Mailhot et Philippe Girard
 % Date de création : 2017-03-16
-% Date d'édition :
+% Date d'édition : 2017-03-19
 % Description du programme : Programme pour la systeme des equations
 % decouples
 
@@ -39,8 +39,7 @@ ZA = 0;
 ZB = 0;
 ZC = 0;
 
-
-
+% Constante d'équilibre trouvees avec SS-2
 dFa_dia_e = 1;
 dFb_dib_e = 1;
 dFc_dic_e = 1;
@@ -69,9 +68,12 @@ rayon_sphere = 3.9/1000;    % Rayon de la sphere en m.
 inertiePx = 1169.1/(1000^2); % Inertie de la plaque en kg*m^2
 inertiePy = inertiePx;
 inertieS = (2*masseS*(rayon_sphere^2))/5; % Inertie de la sphere en kg*m^2
+acc = -(masseS*g)/(masseS+inertieS/(rayon_sphere^2));
 
 Ip = 1169.1; % kg*mm^2
+
 %% Systeme plaque
+
 A13_13 = [0,0,0,1,0,0,0,0,0,0,0,0,0;
           0,0,0,0,1,0,0,0,0,0,0,0,0;
           0,0,0,0,0,1,0,0,0,0,0,0,0;
@@ -112,38 +114,42 @@ C = [YD,-XD,1,0,0,0,0,0,0,0,0,0,0;
 D = zeros(7,3);
 
 Aphi = [A13_13([1 4 11],1),A13_13([1 4 11],4),A13_13([1 4 11],11)];
-
 Atheta = [A13_13([2 5 12],2),A13_13([2 5 12],5),A13_13([2 5 12],12)];
-
 Az = [A13_13([3 6 13],3),A13_13([3 6 13],6),A13_13([3 6 13],13)];
 
-AxS = [A13_13([7 9],7),A13_13([7 9],9)]
-
-AyS = [A13_13([8 10],8),A13_13([8 10],10)]
-
 Bphi = [B13_3([1 4 11],1)];
-
 Btheta = [B13_3([2 5 12],2)];
-
 Bz = [B13_3([3 6 13],3)];
 
-BxS = [B13_3([7 9],1)];
+Cphi = [YD, 0, 0; YE, 0, 0; YF, 0, 0];
+Ctheta = [-XD, 0,0; -XE,0,0; -XF,0,0];
+Cz = [1,0,0;1,0,0;1,0,0];
 
-ByS = [B13_13([]
+Dphi = [0;0;0];
+Dtheta = [0;0;0];
+Dz = [0;0;0];
 
-% to do :petites matrices c et d 
+%State-space pour la plaque
+tf_phi =ss2tf(Aphi, Bphi, Cphi, Dphi);
+tf_theta =ss2tf(Atheta, Btheta, Ctheta, Dtheta);
+tf_z =ss2tf(Az, Bz, Cz, Dz);
+
+%% Systeme sphere
+
+AxS = [A13_13([7 9],7),A13_13([7 9],9)];
+AyS = [A13_13([8 10],8),A13_13([8 10],10)];
+
+BxS = [0, 0; 0, acc];
+ByS = [0, 0; -acc, 0];
+
 % phi est l'entree de y_sphere
-%theta est l'entree de x_sphere
+% theta est l'entree de x_sphere
+CxS = [1, 1; 1,1];
+CyS = [1, 1; 1, 1];
 
+DxS = [0,0;0,0];
+DyS = [0,0;0,0];
 
-%Cphi = [];
-
-%tf_phi =ss2tf(Aphi, Bphi)
-
-
-
-
-
-
-
-
+% State-space pour la sphere
+tf_xs = ss2tf(AxS,BxS,CxS,DxS,2);
+tf_ys = ss2tf(AyS,ByS,CyS,DyS,2);
