128077a83e042241a3a5bcfd49442617b8370a3d
zouloum <jordancareau@hotmail.com>
2017-03-20 15:09:34 -0400

    Correlation avec fft matlab fonctionelle

--- matlab/SI-1/Si_1.m
+++ matlab/SI-1/Si_1.m
@@ -17,12 +17,11 @@ tic
 posX = 138;
 posY = 142;
 
-%Image = Image(:,:,1);%(Image(:,:,1)/3+Image(:,:,2)/3+Image(:,:,3)/3);
-Image = Image(posX-100:posX+100,posY-100:posX+100,1);%(Image(:,:,1)/3+Image(:,:,2)/3+Image(:,:,3)/3);
-Bille = Bille(:,:,1);%(Bille(:,:,1)/3+Bille(:,:,2)/3+Bille(:,:,3)/3);
+Image = im2double(Image(:,:,1));%(Image(:,:,1)/3+Image(:,:,2)/3+Image(:,:,3)/3);
+%Image = Image(posX-100:posX+100,posY-100:posX+100,1);%(Image(:,:,1)/3+Image(:,:,2)/3+Image(:,:,3)/3);
+Bille = im2double(Bille(:,:,1));%(Bille(:,:,1)/3+Bille(:,:,2)/3+Bille(:,:,3)/3);
 %Correlation 2D Normalise
 c = normxcorr2(Bille,Image);
-%c = normxcorr2_mex(double(Bille), double(Image));
 toc
 figure, surf(c), shading flat %Afficher le resultat de la correlation
 
@@ -31,28 +30,45 @@ figure, surf(c), shading flat %Afficher le resultat de la correlation
 yoffSet = ypeak-size(Bille,1); 
 xoffSet = xpeak-size(Bille,2);
 
-%Afficher resultat
-hFig = figure;
-hAx  = axes;
-imshow(Image,'Parent', hAx);
-imrect(hAx, [xoffSet+1, yoffSet+1, size(Bille,2), size(Bille,1)]);
+% %Afficher resultat
+% hFig = figure;
+% hAx  = axes;
+% imshow(Image,'Parent', hAx);
+% imrect(hAx, [xoffSet+1, yoffSet+1, size(Bille,2), size(Bille,1)]);
 
 %% FFT correlation
 % Matrix dimensions
 adim = size(Image);
 bdim = size(Bille);
 % Cross-correlation dimension
-tic
+
 cdim = adim+bdim-1;
 
 bpad = zeros(cdim);
 apad = zeros(cdim);
 
+Bille = Bille - mean2(Bille);
 apad(1:adim(1),1:adim(2)) = Image;
 bpad(1:bdim(1),1:bdim(2)) = Bille(end:-1:1,end:-1:1);
-ffta = fft2(apad);
 fftb = fft2(bpad);
+tic
+ffta = fft2(apad);
+
+
 c = real(ifft2(ffta.*fftb));
+
 toc
 figure, surf(c), shading flat %Afficher le resultat de la correlation
+
+[ypeak, xpeak] = find(c==max(c(:)));
+yoffSet = ypeak-size(Bille,1); 
+xoffSet = xpeak-size(Bille,2);
+hFig = figure;
+hAx  = axes;
+imshow(Image,'Parent', hAx);
+imrect(hAx, [xoffSet+1, yoffSet+1, size(Bille,2), size(Bille,1)]);
+
+
+
+
  
