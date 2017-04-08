c4bef3692d7e1ed6549e99f672b06a6565fb880c
Emile Fugulin <emilefugulin@hotmail.com>
2017-04-08 10:07:11 -0400

    Refactor code and fix problem in algo

---
+++ matlab/ST/interpolation.m
@@ -0,0 +1,42 @@
+function [M, O] = interpolation(N, V, T)
+
+pas = 101; % Pas integration
+
+[coeff] = polyfit(N(1,:),N(2,:),length(N)-1);
+dx = (length(N)-1)/pas;
+x = N(1,1):dx:N(1,end);
+M = [x; polyval(coeff, x)];
+
+% calcul longueur & erreur
+coeff_d = polyder(coeff); % f'
+x_d = polyval(coeff_d, x);
+longueur = sqrt(1 + x_d.^2); % g
+L = trapz(x, longueur);
+
+coeff_d2 = polyder(coeff_d); % f''
+x_d2 = polyval(coeff_d2, x);
+longueur_d = x_d.*x_d2./longueur; %g'
+E = dx^2/2*(longueur_d(end)-longueur_d(1));
+
+% vitesse reelle
+O_taille = ceil(L/(T*V));
+Vreel = L/(T*O_taille);
+
+% distance entre chaque point
+d = L/O_taille;
+
+O = [];
+Ltr = [0];
+xi = 1;
+O = [xi];
+for i = 1:O_taille
+    Ltr(i) = d*(i-1);
+    f = polyval(coeff_d, xi);
+    g = sqrt(1 + f.^2);
+    xi = xi + d/g;
+    O = [O, xi];
+end
+
+O = [O; polyval(coeff, O)];
+
+end
--- matlab/ST/trajectoire.m
+++ matlab/ST/trajectoire.m
@@ -3,70 +3,26 @@ clear all
 close all
 clc
 
-trainSize = 0.5;
-pas = 101; % Pas integration
 T = 1/30; % Periode echantillonage
-V = 1; % vitesse constante
+V = 2; % vitesse constante
 
 aller = [1, 2, 3, 4, 5, 6;4.5, 2.6, 4.5, 2.8, 3.2, 4.6]; % X, Y
 retour = [1, 2, 3, 4, 5, 6;7.4, 10.1, 13, 10, 11, 7.5]; % X, Y
 
 
 %% Trajectoires Aller
-% interpole aller
-[aller_coeff] = polyfit(aller(1,:),aller(2,:),length(aller)-1);
-dx = (length(aller)-1)/pas;
-x = 1:dx:length(aller);
-aller_int = [x; polyval(aller_coeff, x)];
-
-% calcul longueur & erreur
-aller_coeff_d = polyder(aller_coeff); % f'
-aller_d = polyval(aller_coeff_d, x);
-aller_long = sqrt(1 + aller_d.^2); % g
-L = trapz(x, aller_long);
-
-aller_coeff_d2 = polyder(aller_coeff_d); % f''
-aller_d2 = polyval(aller_coeff_d2, x);
-aller_long_d = aller_d.*aller_d2./aller_long; %g'
-E = dx^2/2*(aller_long_d(end)-aller_long_d(1));
-
-% vitesse reelle
-O = ceil(L/(T*V));
-Vreel = L/(T*O);
-
-% distance entre chaque point
-d = L/O;
-
-dx2 = (length(aller)-1)/O;
-x2 = 1:dx2:length(aller);
-g = polyval(aller_coeff_d, x2);
-g = sqrt(1 + g.^2); % g
-points = [];
-Ltr = [0];
-xi = 1;
-for i = 1:O
-    Ltr(i) = d*(i-1);
-    xi = xi + d/g(i);
-    points = [points, xi];
-end
-
-points = [points; polyval(aller_coeff, points)];
+[aller_int,points] = interpolation(aller,V,T);
 
 %% Trajectoire Retour
-% interpole retour
-% [retour_coeff] = polyfit(retour(1,:),retour(2,:),length(retour)-1);
-% retour_int = 1:dx:length(retour);
-% retour_int = [retour_int; polyval(retour_coeff, retour_int)];
-
+[retour_int,points2] = interpolation(retour,V,T);
 
 %% Relier gauche
+% dx = (length(aller)-1)/101;
 % % interpole gauche
 % gauche = [fliplr(aller(:,1:2)) retour(:,1:2)];
 % gauche = [gauche(2,:);gauche(1,:)];
 % 
-% [gauche_coeff] = polyfit(gauche(1,:),gauche(2,:),length(gauche)-1);
-% gauche_int = gauche(1,1):dx:gauche(1,end);
-% gauche_int = [gauche_int; polyval(gauche_coeff, gauche_int)];
+% [gauche_int,points3] = interpolation(gauche,V,T);
 % 
 % [~,half_g] = min(gauche_int(2,:));
 % 
@@ -102,12 +58,13 @@ points = [points; polyval(aller_coeff, points)];
 figure 
 hold on
 plot(aller(2,:), 'o')
+plot(retour(2,:), 'o')
 
 % plot(retour(2,:), 'o')
 plot(aller_int(1,:),aller_int(2,:));
 plot(points(1,:),points(2,:), 'x');
-% plot(retour_int(1,:),retour_int(2,:));
-
+plot(retour_int(1,:),retour_int(2,:));
+plot(points2(1,:),points2(2,:), 'x');
 
 %% Write
 % csvwrite('retour.csv', fliplr(retour_int)')
