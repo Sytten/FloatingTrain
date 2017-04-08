4e91006a33cba329f315460e5b819a5a0f42d635
Philippe Girard <philippegirard@10-238-32-116.aerius.USherbrooke.ca>
2017-04-08 15:56:24 -0400

    yolo ça fonctionne

--- matlab/SC-5/sc5.m
+++ matlab/SC-5/sc5.m
@@ -7,33 +7,51 @@ load('capteur.mat');
 x = distance;
 y = voltage;
 
-err = []
-initial = 1;
-for i = initial:1:100
+starti = 1;
+endi = 100;
 
+y_min_rms = [];
+min_rms = 1000;
+min_rms_index = starti;
+params = [0 0];
+coeffs = [];
+for i = starti:1:endi
     A = 1*i;
-    B = 2*i;
+    B = 4*i;
     
-p1 = ones(size(x));
-p2 = exp(A*x);
-p3 = exp(B*2*x);
+    pxi = [ ones(1,length(x))',...
+            exp(A*x),...
+            exp(B*x)
+            ];
+    
+    R = pinv(pxi)*y;
+    
+    G = 0;
+    for j = 1:size(pxi,2)
+        G = G + R(j)*pxi(:,j);
+    end
 
-PM3 = [p1,p2,p3];
-
-SM3 = pinv(PM3)*y;
-
-
-x = distance;
-ybM3 = SM3(3)*p3+ SM3(2)*p2 + SM3(1);
-% figure 
-% plot(distance,ybM3)
-% hold on
-% plot(distance,voltage)
-
-cr =  rms(voltage-ybM3);
-err = [err cr];
-disp(['i = ', num2str(i), ' rms = ', num2str(cr)])
+    % calculate rms
+    current_rms =  rms(voltage-G);
+    
+    % set smallest rms curve
+    if (current_rms < min_rms)
+        y_min_rms = G;
+        min_rms = current_rms;
+        min_rms_index = i;
+        params = [A B];
+        coeffs = R;
+    end
+    
+    % display error
+%     disp(['i = ', num2str(i), ' rms = ', num2str(current_rms)])
 end
+disp(['minimal rms = ', num2str(min_rms), ' at i = ', num2str(min_rms_index)])
+disp([num2str(coeffs(3)), 'exp(', num2str(params(2)), '*x)+'...
+    num2str(coeffs(2)), 'exp(', num2str(params(1)), '*x)+'...
+    num2str(coeffs(1))])
 
-[me, mi] = min(err);
-disp(['minimal rms = ', num2str(me), ' at i = ', num2str(mi+initial-1)])
+figure 
+plot(distance,y_min_rms)
+hold on
+plot(distance,voltage)
