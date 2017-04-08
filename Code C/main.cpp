faf896ee8c6028534a4361bbb0cafff923f8e7a8
zouloum <jordancareau@hotmail.com>
2017-04-08 12:33:59 -0400

    Padding et conversion en point flotant de l'image du plateau fonctionne

--- Code C/main.cpp
+++ Code C/main.cpp
@@ -1,10 +1,63 @@
+/*
+ * Code d'exemple pour charger une image de format rgb raw.
+ * Exemple:
+ *
+ * load_image image.rgb
+ *
+ */
 
 #include <stdint.h>
 #include "image_processing_plugin.h"
 #include "dummy_image_processing_plugin.cpp"
+#include <boost/shared_array.hpp>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
 
-int main()
+using namespace std;
+
+namespace {
+
+	const unsigned int IMAGE_WIDTH = 480;
+	const unsigned int IMAGE_HEIGHT = 480;
+	const unsigned int IMAGE_SIZE = IMAGE_WIDTH * IMAGE_HEIGHT * 3;
+};
+
+int main( int argc, char **argv)
 {
+	if(argc != 2)
+	{
+		cerr << "Erreur: Vous devez specifier seulement l'image a charger" << endl;
+		return EXIT_FAILURE;
+	}
+
+	// Open image file
+	ifstream image_file(argv[1], ios::binary);
+	if(!image_file)
+	{
+		cerr << "Erreur: Chemin de l'image invalide" << endl;
+		return EXIT_FAILURE;
+	}
+
+	// Check file size
+	image_file.seekg(0, image_file.end);
+	if(image_file.tellg() != IMAGE_SIZE)
+	{
+		cerr << "Erreur: La taille de l'image specifiee est incorrecte" << endl;
+		return EXIT_FAILURE;
+	}
+	image_file.seekg(0, image_file.beg);
+
+	// Read file
+	boost::shared_array<uint8_t> image(new uint8_t[IMAGE_SIZE]);
+	image_file.read(reinterpret_cast<char *>(image.get()), IMAGE_SIZE);
+
+	// Votre code ici: l'image est un tableau lineaire de uint8.
+	// Chaque pixel contient 3 uint8 soit les composantes: Red, Green, Blue (expliquant le "*3" dans IMAGE_SIZE)
+	// Les pixels sont sotckes en mode: row-major order.
+	// L'outil convert de imagemagick peut etre interessant pour convertir le format d'image si requis:
+	// convert -depth 8 -size 480x480 test.rgb test.png
+
 	DummyImageProcessingPlugin* iPP = new DummyImageProcessingPlugin();
 		
 	double x = 1.0;
@@ -12,11 +65,14 @@ int main()
 
 	iPP->OnBallPosition(1.0, 1.0, x, y);
 
+	cout << "red pixel 1: " << (int)(image.get())[2] << endl;
 	
+	iPP->OnImage(image, 480, 480, x, y);
 
 	delete iPP;
-	return 0;
-}
 
+	return EXIT_SUCCESS;
+
+}
 
 
