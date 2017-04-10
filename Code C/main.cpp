8923b1d4a2956f9c4ba169e29bf4896fabdbb516
zouloum <jordancareau@hotmail.com>
2017-04-10 10:29:11 -0400

    Images loaded are now parameters of the main program

--- Code C/main.cpp
+++ Code C/main.cpp
@@ -13,6 +13,8 @@
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
@@ -25,71 +27,61 @@ namespace {
 
 int main( int argc, char **argv)
 {
-	// Open image file
-	ifstream image_file_1("image_718.rgb", ios::binary);
-	ifstream image_file_2("image_751.rgb", ios::binary);
-	ifstream image_file_3("image_785.rgb", ios::binary);
-	ifstream image_file_4("image_818.rgb", ios::binary);
-	ifstream image_file_5("image_852.rgb", ios::binary);
-	ifstream image_file_6("image_884.rgb", ios::binary);
+	if(argc <= 1)
+	{
+		cerr << "Erreur: Vous devez specifier une image a charger" << endl;
+		return EXIT_FAILURE;
+	}
 
-	// Read file
-	boost::shared_array<uint8_t> image_1(new uint8_t[IMAGE_SIZE]);
-	image_file_1.read(reinterpret_cast<char *>(image_1.get()), IMAGE_SIZE);
-	
-	boost::shared_array<uint8_t> image_2(new uint8_t[IMAGE_SIZE]);
-	image_file_2.read(reinterpret_cast<char *>(image_2.get()), IMAGE_SIZE);
-	
-	boost::shared_array<uint8_t> image_3(new uint8_t[IMAGE_SIZE]);
-	image_file_3.read(reinterpret_cast<char *>(image_3.get()), IMAGE_SIZE);
-	
-	boost::shared_array<uint8_t> image_4(new uint8_t[IMAGE_SIZE]);
-	image_file_4.read(reinterpret_cast<char *>(image_4.get()), IMAGE_SIZE);
-	
-	boost::shared_array<uint8_t> image_5(new uint8_t[IMAGE_SIZE]);
-	image_file_5.read(reinterpret_cast<char *>(image_5.get()), IMAGE_SIZE);
-	
-	boost::shared_array<uint8_t> image_6(new uint8_t[IMAGE_SIZE]);
-	image_file_6.read(reinterpret_cast<char *>(image_6.get()), IMAGE_SIZE);
+	DummyImageProcessingPlugin* iPP = new DummyImageProcessingPlugin();	
 
-	// Votre code ici: l'image est un tableau lineaire de uint8.
-	// Chaque pixel contient 3 uint8 soit les composantes: Red, Green, Blue (expliquant le "*3" dans IMAGE_SIZE)
-	// Les pixels sont sotckes en mode: row-major order.
-	// L'outil convert de imagemagick peut etre interessant pour convertir le format d'image si requis:
-	// convert -depth 8 -size 480x480 test.rgb test.png
+	for(int i = 1; i < argc; i++)
+	{
+		// Open image file
+		ifstream image_file(argv[i], ios::binary);
+		if(!image_file)
+		{
+			cerr << "Erreur: Chemin de l'image invalide" << endl;
+			return EXIT_FAILURE;
+		}
 
-	DummyImageProcessingPlugin* iPP = new DummyImageProcessingPlugin();
+		// Check file size
+		image_file.seekg(0, image_file.end);
+		if(image_file.tellg() != IMAGE_SIZE)
+		{
+			cerr << "Erreur: La taille de l'image specifiee est incorrecte" << endl;
+			return EXIT_FAILURE;
+		}
+		image_file.seekg(0, image_file.beg);
+
+
+		// Read file
+		boost::shared_array<uint8_t> image(new uint8_t[IMAGE_SIZE]);
+		image_file.read(reinterpret_cast<char *>(image.get()), IMAGE_SIZE);
+
+		// Votre code ici: l'image est un tableau lineaire de uint8.
+		// Chaque pixel contient 3 uint8 soit les composantes: Red, Green, Blue (expliquant le "*3" dans IMAGE_SIZE)
+		// Les pixels sont sotckes en mode: row-major order.
+		// L'outil convert de imagemagick peut etre interessant pour convertir le format d'image si requis:
+		// convert -depth 8 -size 480x480 test.rgb test.png
+
 		
-	double x = 1.0;
-	double y = 1.0;
 	
-	double vX;
-	double vY;
-	
-	iPP->OnImage(image_1, 480, 480, x, y);
-	iPP->OnBallPosition(x,y,vX,vY);
-	cout << "Vitesse X : " << vX << "  Y: " << vY << endl;
-	
-	iPP->OnImage(image_2, 480, 480, x, y);
-	iPP->OnBallPosition(x,y,vX,vY);
-	cout << "Vitesse X : " << vX << "  Y: " << vY << endl;
+		double x = 1.0;
+		double y = 1.0;
 
-	iPP->OnImage(image_3, 480, 480, x, y);
-	iPP->OnBallPosition(x,y,vX,vY);
-	cout << "Vitesse X : " << vX << "  Y: " << vY << endl;
-	
-	iPP->OnImage(image_4, 480, 480, x, y);
-	iPP->OnBallPosition(x,y,vX,vY);
-	cout << "Vitesse X : " << vX << "  Y: " << vY << endl;
-	
-	iPP->OnImage(image_5, 480, 480, x, y);
-	iPP->OnBallPosition(x,y,vX,vY);
-	cout << "Vitesse X : " << vX << "  Y: " << vY << endl;
-	
-	iPP->OnImage(image_6, 480, 480, x, y);
-	iPP->OnBallPosition(x,y,vX,vY);
-	cout << "Vitesse X : " << vX << "  Y: " << vY << endl;
-	
+		double vX;
+		double vY;
+
+
+		iPP->OnImage(image, 480, 480, x, y);
+		iPP->OnBallPosition(x,y,vX,vY);
+		cout << "Vitesse X : " << vX << "  Y: " << vY << endl;
+
+
+		
+	}
+
 	delete iPP;
 
 	return EXIT_SUCCESS;
