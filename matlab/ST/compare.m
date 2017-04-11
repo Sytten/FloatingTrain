cec43e21dab5887e03b2286e24c7278ce603ff8e
Philippe Girard <philippe.girard5@gmail.com>
2017-04-11 11:34:41 -0400

    add official paths

--- matlab/ST/compare.m
+++ matlab/ST/compare.m
@@ -2,10 +2,11 @@ clear all
 close all
 clc
 
-position_commande = csvread('aller.csv');
-position_mesure = csvread('aller_interpoler.csv');
-position_mesure = position_mesure./0.05.*4;
+position_commande = csvread('trajectoireofficielle/aller_interpole.csv')';
+position_mesure = csvread('trajectoireofficielle/trajectoire_aller_asservie.csv')';
 
+dx = length(position_mesure)/length(position_commande);
+position_down = position_mesure(1:dx:end,1:2);
 %% Display
 figure
 hold on
@@ -13,12 +14,12 @@ plot(position_commande(:,1), position_commande(:,2))
 plot(position_mesure(:,1), position_mesure(:,2))
 
 %% Reduce vector
-position_down = [];
-for i = 1:length(position_commande)
-    x = position_commande(i,1);
-    idx = find(position_mesure > x,1);
-    position_down = [position_down; position_mesure(idx,:)];
-end
+% position_down = [];
+% for i = 1:length(position_commande)
+%     x = position_commande(i,1);
+%     idx = find(position_mesure > x,1);
+%     position_down = [position_down; position_mesure(idx,:)];
+% end
 
 %% Display down
 figure
