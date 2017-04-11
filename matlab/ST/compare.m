366bb50c097901260a2e2b32b404ae492e3b8da6
Emile Fugulin <emilefugulin@hotmail.com>
2017-04-11 14:20:15 -0400

    Fix compare

--- matlab/ST/compare.m
+++ matlab/ST/compare.m
@@ -11,22 +11,22 @@ position_down = position_mesure(1:dx:end,1:2);
 figure
 hold on
 plot(position_commande(:,1), position_commande(:,2))
-plot(position_mesure(:,1), position_mesure(:,2))
+plot(position_mesure(:,1), position_mesure(:,2), 'r')
 
 %% Reduce vector
-% position_down = [];
-% for i = 1:length(position_commande)
-%     x = position_commande(i,1);
-%     idx = find(position_mesure > x,1);
-%     position_down = [position_down; position_mesure(idx,:)];
-% end
+coeff = polyfit(position_mesure(:,1), position_mesure(:,2), 20);
+position_down = [position_commande(:,1), polyval(coeff, position_commande(:,1))];
+position_down = [position_down, polyval(polyder(coeff), position_down(:,1))];
 
 %% Display down
 figure
 hold on
 plot(position_commande(:,1), position_commande(:,2))
-plot(position_down(:,1), position_down(:,2))
+plot(position_down(:,1), position_down(:,2), 'r')
 
 %% Performance
 Distance = sqrt((position_commande(:,1)-position_down(:,1)).^2 + (position_commande(:,2)-position_down(:,2)).^2);
-Performance = sqrt(trapz(Distance.^2));
+Performance = [];
+for i = 1:length(Distance)
+    Performance = [Performance; sqrt(trapz(Distance(1:i).^2))];
+end
